cc27e21bf5b97dad06b086cf2f715469490cab36
Pickle Dan <PickleDan@yandex.ru>
2020-03-05 16:59:51 +0300

    add adaptive css layout

--- src/components/CurrentTask.js
+++ src/components/CurrentTask.js
@@ -8,13 +8,15 @@ function CurrentTask() {
         <h3>Название</h3>
         <p>Верстка главной страницы</p>
       </div>
-      <div className="current-task-item">
-        <h3>Начало</h3>
-        <p>12:10:15</p>
-      </div>
-      <div className="current-task-item">
-        <h3>Длительность</h3>
-        <p>01.09.02</p>
+      <div className="task-duration">
+        <div className="current-task-item">
+          <h3>Начало</h3>
+          <p>12:10:15</p>
+        </div>
+        <div className="current-task-item">
+          <h3>Длительность</h3>
+          <p>01.09.02</p>
+        </div>
       </div>
       <Button btnName={"Закончить работу"} />
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,8 @@ function Header() {
   return (
     <div className="header">
       <div className="container header-flex">
-        <div>Учет времени работы</div>
-        <div>
+        <h1>Учет времени работы</h1>
+        <div className="current-time">
           Текущее время <Clock />
         </div>
       </div>
